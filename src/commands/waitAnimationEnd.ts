8f7cdcdc8c3861d1251b9e118bc5c7a89599cadb
Caio Teixeira <caio.teixeira@unyleya.com.br>
2024-05-21 12:35:24 -0300

    fix: waitAnimationEnd

--- src/commands/waitAnimationEnd.ts
+++ src/commands/waitAnimationEnd.ts
@@ -23,7 +23,7 @@ export default async function waitAnimationEnd(selector = 'body') {
         const handler = () => {
           if (done) return;
           done = true;
-          setTimeout(() => resolve(true), 0);
+          resolve(true);
         };
 
         $el.addEventListener('animationend', handler, { once: true });
